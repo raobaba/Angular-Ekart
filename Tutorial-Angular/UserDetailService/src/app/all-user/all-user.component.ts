c7e08818e6ea7ffd11c6b1108e14084f52ddfd90
raobaba <raorajan9576@gmail.com>
2023-05-20 11:26:21 +0530

    UserDetailService

--- Tutorial-Angular/UserDetailService/src/app/all-user/all-user.component.ts
+++ Tutorial-Angular/UserDetailService/src/app/all-user/all-user.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../Services/user.service';
 
 @Component({
   selector: 'app-all-user',
   templateUrl: './all-user.component.html',
   styleUrls: ['./all-user.component.css']
 })
-export class AllUserComponent {
+export class AllUserComponent implements OnInit {
+
+  constructor(private userService: UserService) { }
+
+  users: {name: string, job: string, gender: string, country: string, age: number, avatar: string}[] = [];
+
+  ngOnInit(): void {
+    this.users = this.userService.users;
+  }
+
+  ShowDetails(user: {name: string, job: string, gender: string, country: string, age: number, avatar: string}){
+    this.userService.ShowUserDetails(user);
+  }
 
 }
--- Tutorial-Angular/UserDetailService/src/app/app.module.ts
+++ Tutorial-Angular/UserDetailService/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServicesComponent } from './services/services.component';
 import { AllUserComponent } from './all-user/all-user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServicesComponent,
     AllUserComponent,
     UserDetailComponent
   ],
