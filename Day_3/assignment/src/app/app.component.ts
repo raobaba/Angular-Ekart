9381c4669b1e45105abcfe45d3d55dba66776cc9
raobaba <raorajan9576@gmail.com>
2023-05-31 07:25:35 +0530

    complete assignmnet

--- Day_3/assignment/src/app/app.component.ts
+++ Day_3/assignment/src/app/app.component.ts
@@ -2,48 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <app-topbar/>
+  <app-header/>
+  <app-main/>
+  <app-footer/>
+  `
 })
 export class AppComponent {
-  title = 'assignmnet';
-  boxes = [
-    {
-      thumbnail: 'path-to-thumbnail-image1.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    },
-    {
-      thumbnail: 'path-to-thumbnail-image2.jpg',
-      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-    }
-    // Add more box objects as needed
-  ];
+  title = 'assignment';
 }
